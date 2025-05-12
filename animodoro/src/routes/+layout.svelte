d792ac6b16176ee596d1da5db323a19c9c2f9683
codeintheshell <thibault.castelli@pm.me>
2025-05-12 14:36:42 -0400

    Add theme toggle

--- animodoro/src/routes/+layout.svelte
+++ animodoro/src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { ModeWatcher } from "mode-watcher";
+	import ThemeToggle from '$lib/components/ui/themeToggle/themeToggle.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
+	<ModeWatcher />
 <ParaglideJS {i18n}>
+	<ThemeToggle />
 	{@render children()}
 </ParaglideJS>
