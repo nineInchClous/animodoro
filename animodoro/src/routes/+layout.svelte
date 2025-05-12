530cb3d531dba62b44d9fed4f90ae9fb174e955b
codeintheshell <thibault.castelli@pm.me>
2025-05-12 15:18:24 -0400

    Add locale toggle

--- animodoro/src/routes/+layout.svelte
+++ animodoro/src/routes/+layout.svelte
@@ -2,13 +2,16 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import { ModeWatcher } from "mode-watcher";
-	import ThemeToggle from '$lib/components/ui/themeToggle/themeToggle.svelte';
+	import ThemeToggle from '$lib/components/ui/theme-toggle/theme-toggle.svelte';
+	import LocaleToggle from '$lib/components/ui/locale-toggle/local-toggle.svelte';
 	import '../app.css';
+
 	let { children } = $props();
 </script>
 
-	<ModeWatcher />
 <ParaglideJS {i18n}>
+	<ModeWatcher />
 	<ThemeToggle />
+	<LocaleToggle />
 	{@render children()}
 </ParaglideJS>
