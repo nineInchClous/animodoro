7f191e6fadcac49ca521feed99ee362e02fe6c41
codeintheshell <thibault.castelli@pm.me>
2025-05-17 16:16:31 -0400

    Fix timer context key

--- animodoro/src/lib/contexts/timerContext.svelte.ts
+++ animodoro/src/lib/contexts/timerContext.svelte.ts
@@ -1,12 +1,12 @@
 ﻿import { getContext, setContext } from 'svelte';
 import type {Timer} from "$lib/entities/timer.svelte";
 
-const TIMER_KEY = 'timer';
+const key = {};
 
 export function setTimerContext(timer: Timer) {
-	setContext(TIMER_KEY, timer);
+	setContext(key, timer);
 }
 
 export function getTimerContext() {
-	return getContext(TIMER_KEY) as Timer;
+	return getContext(key) as Timer;
 }
