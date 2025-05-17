14cbc1243d8abf1245770032d84159148e700b2a
codeintheshell <thibault.castelli@pm.me>
2025-05-17 19:56:19 -0400

    Add error page styling

--- animodoro/src/routes/+error.svelte
+++ animodoro/src/routes/+error.svelte
@@ -1,13 +1,47 @@
 ﻿<script lang="ts">
 	import { page } from '$app/state';
+	import DogImage from '$lib/assets/images/undraw_page-eaten_b2rt.svg';
 </script>
 
-<div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-	{#if page.status === 404}
+<div class="simple-container">
+	<div>
 		<h1>Oops...</h1>
-		<p>The page you're looking for doesn't exist.</p>
-		<p>Please check your URL or return to the <a href="/">Home Page</a></p>
-	{:else}
-		<h1>{page.status} {page.error?.message}</h1>
-	{/if}
+		{#if page.status === 404}
+			<p>The page you're looking for doesn't exist.</p>
+			<p>
+				Please check your URL or return to the <a href="/">Home Page</a>
+			</p>
+		{:else}
+			<p>{page.status} {page.error?.message}</p>
+			<p>
+				Please return to the <a href="/">Home Page</a>
+			</p>
+		{/if}
+		<img alt="Dog and eaten papers" src={DogImage} />
+	</div>
 </div>
+
+<style lang="postcss">
+	.simple-container {
+		@apply flex items-center justify-center;
+		div {
+			@apply text-center;
+
+			h1 {
+				@apply my-4 text-4xl font-bold;
+			}
+
+			p {
+				@apply mb-2;
+
+				a {
+					@apply text-blue-500 hover:underline;
+				}
+			}
+
+			img {
+				@apply mx-auto mt-10 max-w-40;
+			}
+		}
+	}
+</style>
--- animodoro/src/routes/+page.svelte
+++ animodoro/src/routes/+page.svelte
@@ -17,7 +17,7 @@
 	});
 </script>
 
-<div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+<div class="simple-container">
 	<TimerDisplay />
 	<PreviousButton />
 	<PlayPauseButton />
