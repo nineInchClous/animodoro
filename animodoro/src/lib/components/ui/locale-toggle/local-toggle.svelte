fea32c905e64064a2800a0a5e5d731b6ef8982bd
codeintheshell <thibault.castelli@pm.me>
2025-05-17 16:16:19 -0400

    Fix local toggler

--- animodoro/src/lib/components/ui/locale-toggle/local-toggle.svelte
+++ animodoro/src/lib/components/ui/locale-toggle/local-toggle.svelte
@@ -1,20 +1,26 @@
 ﻿<script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { languageTag, setLanguageTag } from '$lib/paraglide/runtime.js';
+	import { languageTag } from '$lib/paraglide/runtime.js';
+	import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
+	import { i18n } from '$lib/i18n';
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
 
 	let currentLanguageTag = $state(languageTag());
 
 	const toggleLocale = () => {
-		if (languageTag() === 'en') {
-			setLanguageTag('fr');
-			currentLanguageTag = 'fr';
-		} else {
-			setLanguageTag('en');
-			currentLanguageTag = 'en';
-		}
+		const newLanguage = currentLanguageTag === 'en' ? 'fr' : 'en';
+		currentLanguageTag = newLanguage;
+		switchToLanguage(newLanguage);
 	};
+
+	function switchToLanguage(newLanguage: AvailableLanguageTag) {
+		const canonicalPath = i18n.route(page.url.pathname);
+		const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
+		goto(localisedPath);
+	}
 </script>
 
 <Button variant="outline" size="icon" onclick={toggleLocale}>
-	{currentLanguageTag === 'en' ? 'FR' : 'EN'}
+	{currentLanguageTag}
 </Button>
